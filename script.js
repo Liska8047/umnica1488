6a69b20c647e8c7482135ae99a8992b174d48271
Liza Pivovarova <Liska_Pivovarova228@mail.ru>
2020-04-01 22:22:39 +0400

    коммит

---
+++ script.js
@@ -0,0 +1,27 @@
+let money = 50000;
+let income = "фриланс";
+let addExpenses = "Такси";
+let deposit = false;
+let mission = 500000;
+let period = 5;
+
+alert ("я точно дебил");
+
+console.log ("или не дебил");
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+
+console.log(addExpenses.length);
+
+console.log("Период равен " + period +  " месяцев");
+
+console.log("Цель заработать " + mission +  " рублей");
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split( " "));
+
+ let budgetDay = money / 30;
+ console.log(budgetDay);
