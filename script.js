e7583fbfeece8f536c86fc5109907d9400ffd5bb
Liza Pivovarova <Liska_Pivovarova228@mail.ru>
2020-04-02 15:39:58 +0400

    и это сделаль

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let money = 50000;
 let income = "фриланс";
-let addExpenses = "Такси";
+let addExpenses = "Такси, свет, газ";
 let deposit = false;
 let mission = 500000;
 let period = 5;
@@ -21,7 +21,7 @@ console.log("Период равен " + period +  " месяцев");
 console.log("Цель заработать " + mission +  " рублей");
 
 console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split( " "));
+console.log(addExpenses.split( ", "));
 
  let budgetDay = money / 30;
  console.log(budgetDay);
